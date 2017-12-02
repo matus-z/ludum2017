0963f5f6ed46c6e6d2f7b53af050111b1c473994
Matus Zdansky <matuszdansky@gmail.com>
2017-12-02 15:48:47 +0100

    Layers and SceneController

--- BestTripEver/Assets/scripts/Layers.cs
+++ BestTripEver/Assets/scripts/Layers.cs
@@ -2,15 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Layers : MonoBehaviour {
+// ----------------------------------------------------------------
+public class Layers : MonoBehaviour
+{
+    public enum ELayer
+    {
+        Base,
+        Red,
+        Green,
+        Blue,
+        Yellow,
+    };
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject Base;
+    public GameObject Red;
+    public GameObject Green;
+    public GameObject Blue;
+    public GameObject Yellow;
+
+    private ELayer ActLayer;
+    private Dictionary<ELayer, GameObject> LayerDict;
+
+    // ----------------------------------------------------------------
+    private void Start ()
+    {
+        Init();
+        DeactivateAll();
+        ActivateLayer(ELayer.Base);
+    }
+
+    // ----------------------------------------------------------------
+    private void Init()
+    {
+        LayerDict = new Dictionary<ELayer, GameObject>();
+
+        LayerDict.Add(ELayer.Base, Base);
+        LayerDict.Add(ELayer.Red, Red);
+        LayerDict.Add(ELayer.Green, Green);
+        LayerDict.Add(ELayer.Blue, Blue);
+        LayerDict.Add(ELayer.Yellow, Yellow);
+    }
+
+    // ----------------------------------------------------------------
+    // Deactivate all layers
+    private void DeactivateAll()
+    {
+        foreach (KeyValuePair<ELayer, GameObject> LayObj in LayerDict)
+            if(LayObj.Value != null)
+                LayObj.Value.SetActive(false);
+    }
+
+    // ----------------------------------------------------------------
+    public void ActivateLayer(ELayer layer)
+    {
+        ActLayer = layer;
+
+        GameObject lo = null;
+        bool isObject = LayerDict.TryGetValue(layer, out lo);
+        if (isObject == false || lo == null)
+            return;
+
+        // Dactivate all except
+        foreach (KeyValuePair<ELayer, GameObject> LayObj in LayerDict)
+            if(LayObj.Value != null)
+                LayObj.Value.SetActive(layer == LayObj.Key);
+    }
 }
---
+++ BestTripEver/Assets/scripts/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ----------------------------------------------------------------
+public class SceneController : MonoBehaviour
+{
+    private List<Layers.ELayer> UnlockedLayers;
+
+    // ----------------------------------------------------------------
+	private void Start ()
+    {
+        UnlockedLayers = new List<Layers.ELayer>();
+        UnlockedLayers.Add(Layers.ELayer.Base);
+	}
+
+    // ----------------------------------------------------------------
+    private void Update()
+    {
+        ActivateLayer(Layers.ELayer.Red);
+    }
+
+    // ----------------------------------------------------------------
+    public void ActivateLayer(Layers.ELayer layer)
+    {
+        UnlockedLayers.Add(layer);
+        foreach (Transform child in transform)
+            child.gameObject.GetComponent<Layers>().ActivateLayer(layer);
+    }
+}
